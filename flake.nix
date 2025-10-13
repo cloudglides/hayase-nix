7b556aba87e705faaf13978356ce3d49cf411dc8
cloudglides <authqtx@gmail.com>
2025-10-14 00:21:07 +0530

    init

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Hayase - Stream anime torrents, real-time with no waiting";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      default = pkgs.callPackage ./package.nix {};
+    });
+
+    overlays.default = final: prev: {
+      hayase = self.packages.${final.system}.default;
+    };
+  };
+}
---
+++ package.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  appimageTools,
+}: let
+  pname = "hayase";
+  version = "6.4.34";
+
+  src = fetchurl {
+    url = "https://github.com/hayase-app/ui/releases/download/v${version}/linux-hayase-${version}-linux.AppImage";
+    hash = "sha256-P5xTTcw2fgnv/eGG4bpFKywu4w2Cl7Bj1/BA8nKnMDA=";
+  };
+
+  appimageContents = appimageTools.extractType2 {
+    inherit pname version src;
+  };
+in
+  appimageTools.wrapType2 {
+    inherit pname version src;
+
+    extraInstallCommands = ''
+      install -Dm444 ${appimageContents}/hayase.desktop -t $out/share/applications
+      substituteInPlace $out/share/applications/hayase.desktop \
+        --replace 'Exec=AppRun' 'Exec=${pname}'
+
+      install -Dm444 ${appimageContents}/hayase.png \
+        $out/share/icons/hicolor/512x512/apps/hayase.png
+    '';
+
+    meta = with lib; {
+      description = "Stream anime torrents instantly, real-time with no waiting for downloads to finish";
+      homepage = "https://hayase.watch";
+      license = licenses.bsl11;
+      mainProgram = "hayase";
+      platforms = ["x86_64-linux" "aarch64-linux"];
+    };
+  }
